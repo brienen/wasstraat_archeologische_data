6df3a600a004720de2d5385bf5e051b2767d4f35
Arjen Brienen <brienen@e-space.nl>
2022-02-04 13:38:55 +0100

    Removed Dash en set back logging level

--- app/app/__init__.py
+++ app/app/__init__.py
@@ -8,28 +8,13 @@ from flask_dropzone import Dropzone
 from app.blueprints.gridfs_flask_blueprint import gridfs
 from app.index import MyIndexView
 from flask_debugtoolbar import DebugToolbarExtension
-import dash
-from app.cockpit.dash_cockpit import layout
-from app.cockpit.dash_cockpit import register_callbacks
-from app.cockpit.dash_cockpit import external_stylesheets, external_scripts
 
 logging.basicConfig(format="%(asctime)s:%(levelname)s:%(name)s:%(message)s")
-logging.getLogger().setLevel(logging.INFO)
+logging.getLogger().setLevel(logging.WARNING)
 
 app = Flask(__name__)
 app.config.from_object("config")
 dropzone = Dropzone(app)
-
-#dashapp = dash.Dash(
-#    __name__,
-#    server=app,
-#    external_stylesheets=external_stylesheets,
-#    external_scripts=external_scripts,
-#    routes_pathname_prefix='/cockpit/'
-#)
-#dashapp.layout = layout
-#register_callbacks(dashapp)
-
 
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 toolbar = DebugToolbarExtension(app)
